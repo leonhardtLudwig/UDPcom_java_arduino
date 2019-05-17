623fd3bd4a08abe0c9c5dbca5da05142c0d2e5a4
Leonardo Luigi <leo2852.info@gmail.com>
2019-05-17 17:36:12 +0200

    update

--- client_java/src/Main.java
+++ client_java/src/Main.java
@@ -1,24 +1,94 @@
 import java.io.*;
 import java.net.*;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        while(true) {
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
+    private static byte[] sendData;
+    private static byte[] receiveData;
+    private static InetAddress IPAddress;
+    private static DatagramSocket clientSocket;
+    private static boolean hasBeenSetupped = false;
 
-            String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData());
-            System.out.println("FROM SERVER:" + modifiedSentence);
+    public static void main(String[] args) throws Exception{
+        clientSocket = new DatagramSocket();
+        IPAddress = InetAddress.getByName("localhost");
+        while(!hasBeenSetupped) {
+
+            sendData = new byte[1024];
+            receiveData = new byte[1024];
+
+            sendRequest("GETDATE");
+            getResponse();
+            hasBeenSetupped = isDate();
+            Thread.sleep(2000);
             //clientSocket.close();
         }
+        System.out.println("Client Setup Done!");
+        while(true){
+
+        }
     }
+
+
+    private static boolean isDate(){
+        //dd/MM/yyyy_HH:mm:ss
+        String rec = new String(receiveData);
+        String format = "dd/MM/yyyy_HH:mm:ss";
+            if (rec.indexOf('/') == format.indexOf('/')) {
+                if (rec.indexOf('/', rec.indexOf('/')) == format.indexOf('/', format.indexOf('/'))) {
+                    if (rec.indexOf('_') == format.indexOf('_')) {
+                        if (rec.indexOf(':') == format.indexOf(':')) {
+                            if (rec.indexOf(':',rec.indexOf(':'))==format.indexOf(':',format.indexOf(':'))) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+    }
+
+    private static boolean bytesCompare(byte[]data1,byte[]data2){
+        //System.out.println(new String(data1)+new String(data2));
+        boolean doesItMatch = false;
+        for(int i = 0; !doesItMatch && (i<data1.length && i<data2.length);i++){
+            doesItMatch = data2[i]==data1[i];
+        }
+        return doesItMatch;
+    }
+
+    private static void sendRequest(String r){
+        sendData = r.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        try{
+            System.out.println("Sending: "+(new String(sendData)));
+            clientSocket.send(sendPacket);
+        }catch (IOException e){
+            System.out.println("ERROR!DATAGRAM HAS NOT BEEN SENT");
+        }
+    }
+
+    private static String getResponse(){
+        String response = null;
+        try {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            response = new String(receivePacket.getData());
+            receiveData = receivePacket.getData();
+            System.out.println("FROM SERVER:" + response);
+        }catch (IOException e){
+            System.out.println("No datagram received");
+        }
+        return response;
+    }
+
 }
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,31 +1,71 @@
+import java.io.IOException;
 import java.io.*;
 import java.net.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 public class Main {
 
     private static boolean setupRequest = false;
+    private static final byte[] getDate = {'G','E','T','D','A','T','E'};
+    private static byte[] receiveData;
+    private static byte[] sendData;
+    private static DatagramSocket serverSocket;
+    private static DatagramPacket receivePacket;
+
 
     public static void main(String[] args) throws Exception{
-        DatagramSocket serverSocket = new DatagramSocket(9876);
+        serverSocket = new DatagramSocket(9876);
 
         while(true)
         {
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            receiveData = new byte[1024];
+            sendData = new byte[1024];
+            receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
+            //String clientRequest = new String( receivePacket.getData());
+            byte[] requestData = receivePacket.getData();
+            System.out.println(new String(requestData));
+            if(bytesCompare(requestData,getDate)){
+                sendDate();
+            }else{
+                sendResponse("WRONG REQUEST");
+            }
 
-            setupRequest = sentence.equals("GETDATE");
-            System.out.println(sentence.equals("GETDATE"));
-            //if(setupRequest) {
-                System.out.println("RECEIVED: " + sentence);
-                InetAddress IPAddress = receivePacket.getAddress();
-                int port = receivePacket.getPort();
-                String capitalizedSentence = sentence.toUpperCase();
-                sendData = capitalizedSentence.getBytes();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                serverSocket.send(sendPacket);
-            //}
         }
     }
+
+    private static boolean bytesCompare(byte[]data1,byte[]data2){
+        boolean doesItMatch = false;
+        for(int i = 0; !doesItMatch && (i<data1.length && i<data2.length);i++){
+            doesItMatch = data2[i]==data1[i];
+        }
+        return doesItMatch;
+    }
+
+    private static void sendResponse(String s){
+        InetAddress IPAddress = receivePacket.getAddress();
+        int port = receivePacket.getPort();
+        //String capitalizedSentence = r.toUpperCase();
+        sendData = s.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        //serverSocket.send(sendPacket);
+        try{
+            serverSocket.send(sendPacket);
+            System.out.println("DATAGRAM SENT");
+        }catch (IOException e){
+            System.out.println("ERROR!DATAGRAM HAS NOT BEEN SENT");
+        }
+    }
+
+    private static String server_CurrentDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy_HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+    private static void sendDate(){
+        String date = server_CurrentDate();
+        sendResponse(date);
+    }
+    
 }
